92922b77dd86c06b7e9be2d0abd9e437d9a003ba
thy00 <floatthy@163.com>
2018-06-02 01:11:38 +0800

    使用dubbo进行商品查询及分页 第二次提交

--- taotao-manager-web/src/main/java/cn/thyonline/taotao/controller/ItemController.java
+++ taotao-manager-web/src/main/java/cn/thyonline/taotao/controller/ItemController.java
@@ -3,6 +3,7 @@ package cn.thyonline.taotao.controller;
 import cn.thyonline.taotao.common.pojo.EasyUIDataGridResult;
 import cn.thyonline.taotao.service.ItemService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
 
@@ -14,6 +15,7 @@ public class ItemController {
     /**
      * 响应商品列表
      */
+    @RequestMapping("/item/list")
     public EasyUIDataGridResult getItemList(Integer page,Integer rows){
         EasyUIDataGridResult result = itemService.getItemList(page, rows);
         return result;
